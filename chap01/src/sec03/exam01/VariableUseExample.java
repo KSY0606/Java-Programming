c4fb10a9cb800acc54d4b0d4d7ef41a0d7e743c6
sangyun kim <tkddbs9506@naver.com>
2022-01-23 22:20:16 +0900

    java

---
+++ chap01/src/sec03/exam01/VariableUseExample.java
@@ -0,0 +1,18 @@
+package sec03.exam01;
+
+public class VariableUseExample {
+
+	public static void main(String[] args) {
+		int hour = 3;
+		int minute = 30;
+		System.out.println(hour + "½Ã°£" + minute + "ºÐ");
+		
+		int totalminute = (hour*60) + minute;
+		System.out.println("ÃÑ" + totalminute + "ºÐ");
+		
+		
+				
+
+	}
+
+}
