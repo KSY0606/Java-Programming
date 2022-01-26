7e9823534865cd060bfcedccaf52d647725e8200
sangyun kim <tkddbs9506@naver.com>
2022-01-26 17:02:37 +0900

    Java

---
+++ chap01/src/sec03/exam04/LongOperationExample.java
@@ -0,0 +1,23 @@
+package sec03.exam04;
+
+public class LongOperationExample {
+
+	public static void main(String[] args) {
+		
+		byte value1 = 10;
+		int value2 = 100;
+		long value3 = 1000L;
+		
+		long result = value1 + value2 + value3;
+		
+		System.out.println(result);
+		
+		int x = 1;
+		int y = 2;
+		double result3 = (double) x / y;
+		System.out.println(result3);
+		
+	}
+
+	
+}
