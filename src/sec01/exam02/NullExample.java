90ab8bfeb184a80bc1a99430477c9fb2ff972997
sangyun kim <tkddbs9506@naver.com>
2022-02-04 15:33:14 +0900

    Java 참조타입

---
+++ src/sec01/exam02/NullExample.java
@@ -0,0 +1,14 @@
+package sec01.exam02;
+
+public class NullExample {
+
+	public static void main(String[] args) {
+		
+		String hobby = null;
+		
+		String name = "홍자바";
+		name = null;			// name은 홍자바를 참조했지만 이참조를 없애고 null로 저장함.
+
+	}
+
+}
